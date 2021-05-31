aa5772146b3795ecfbfacfc6dba22ce07615fc64
sonhoaian <sonhoaian1991@gmail.com>
2021-05-31 23:32:14 +0700

    Second cobol project

--- COBOL/HelloWolrd.cbl
+++ COBOL/HelloWolrd.cbl
@@ -4,16 +4,6 @@
       * Purpose:
       * Tectonics: cobc
       ******************************************************************
-      * IDENTIFICATION DIVISION.
-      * PROGRAM-ID. YOUR-PROGRAM-NAME.
-      * DATA DIVISION.
-      * FILE SECTION.
-      * WORKING-STORAGE SECTION.
-      * PROCEDURE DIVISION.
-      * MAIN-PROCEDURE.
-      *      DISPLAY "Hello world"
-      *      STOP RUN.
-      * END PROGRAM YOUR-PROGRAM-NAME.
        IDENTIFICATION DIVISION.
        PROGRAM-ID. MyFirstConsoleApp.
        DATA DIVISION.
