698a378dec29b855f6cef710effc8bd36b67e041
sonhoaian <sonhoaian1991@gmail.com>
2021-06-02 00:44:33 +0700

    new change in 2_InNOutjl

--- JULIA/2_InNOut.jl
+++ JULIA/2_InNOut.jl
@@ -2,8 +2,8 @@ using Base: String
 using Printf
 #declare function
 function main()
-    println("input your user name")
-    name::String = readline();
+    println("input your username")
+    name::String = readline()
     @printf("your name is: %s", name)
 end
 
